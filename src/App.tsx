9c9bf9581c583bd6ed709b950d45758e84ddd136
Malik Ali <malikaalii007@gmail.com>
2026-04-22 17:20:59 +0500

    Component split - separate DesktopModel and MobileModel to fix iOS Safari

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect, useState } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import "./App.css";
 
 const CharacterModel = lazy(() => import("./components/Character"));
@@ -43,26 +43,7 @@ const App = () => {
           </div>
         }>
           <MainContainer>
-            <Suspense fallback={
-              <div style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}>
-                <div style={{
-                  width: '30px',
-                  height: '30px',
-                  border: '2px solid #f3f3f3',
-                  borderTop: '2px solid #3498db',
-                  borderRadius: '50%',
-                  animation: 'spin 1s linear infinite'
-                }}></div>
-              </div>
-            }>
+            <Suspense fallback={null}>
               <CharacterModel />
             </Suspense>
           </MainContainer>
